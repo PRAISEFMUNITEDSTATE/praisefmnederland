fa055540ca3d9a202097f52db2ed909d3a62c8c3
Global Woship Network <globalworshipnetworkusa@gmail.com>
2026-07-06 19:00:03 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(),
-
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'icon-192x192.png', 'icon-512x512.png'],
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'icon-192.png',
+        'icon-512.png'
+      ],
       manifest: {
         name: 'Praise FM Nederland',
         short_name: 'Praise FM NL',
@@ -19,13 +20,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: '/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/icon-512x512.png',
+            src: '/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
@@ -33,33 +34,5 @@ export default defineConfig({
         ]
       }
     })
-  ],
-
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (
-              id.includes('react') ||
-              id.includes('react-dom') ||
-              id.includes('react-router-dom')
-            ) {
-              return 'react'
-            }
-
-            if (
-              id.includes('lucide-react') ||
-              id.includes('swiper')
-            ) {
-              return 'ui'
-            }
-
-            return 'vendor'
-          }
-        }
-      }
-    }
-  }
+  ]
 })
